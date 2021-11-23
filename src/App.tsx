9f2a259f3e63565dadfff9ea663f5465095034fb
Nicole Janeway Bills <nicolejanewaybills@gmail.com>
2021-11-23 16:07:16 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+require("dotenv").config();
 
 function App() {
+  console.log(process.env);
+
   return (
     <div className="App">
       <header className="App-header">
