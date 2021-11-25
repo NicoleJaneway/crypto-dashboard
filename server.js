0016d5a06150adc38b7a0f71f387491e88b51129
Nicole Janeway Bills <nicolejanewaybills@gmail.com>
2021-11-25 11:06:48 -0600

    testing

--- server.js
+++ server.js
@@ -13,4 +13,6 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
+// app.post("/cryptolist", function )
+
 app.listen(process.env.PORT || 8080);
