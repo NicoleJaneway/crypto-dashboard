41874311c65c0e5349f75b84ff985be24e54b4e3
Nicole Janeway Bills <nicolejanewaybills@gmail.com>
2021-11-26 11:30:04 -0600

    search

--- src/components/CryptoSearch.tsx
+++ src/components/CryptoSearch.tsx
@@ -1,9 +1,16 @@
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-
 import { useState, useEffect } from "react";
 import useFetch from "../helpers/useFetch";
 import Loader from "../helpers/Loader";
+
+import Box from "@mui/material/Box";
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 export default function CryptoSearch() {
   const { get, loading } = useFetch("https://api.coingecko.com/api/v3/");
@@ -19,53 +26,56 @@ export default function CryptoSearch() {
 
   useEffect(() => {
     get("coins/list")
-      .then((data: string[]) => {
-        setCryptos(data);
+      .then((data: { id: string; symbol: string; name: string }[]) => {
+        console.log(data);
+        const input = data.filter((obj) => obj.id === "bitcoin");
+        console.log(input);
+        setCryptos(input);
       })
       .catch((error) => console.log("Could not load crypto", error));
   }, []);
 
-  useEffect(() => {
-    console.log(searchedCrypto);
-    setCleanedQuery(searchedCrypto.trim().toLowerCase());
-  }, [searchedCrypto]);
-
-  useEffect(() => {
-    if (cryptos.length > 0) {
-      let searchString = ""
-
-      cryptos.forEach((obj) => {
-        searchString = obj.id + " " + obj.name + " " + obj.symbol 
-        searchData.push(searchString)
-      });
-    }
-  }, [cryptos]);
-
   return (
     <>
-      {loading && <Loader />}
       <Box
         component="form"
         sx={{
-          "& > :not(style)": { m: 1, width: "25ch" },
+          "& > :not(style)": { m: 1, width: "35ch" },
         }}
         noValidate
         autoComplete="off"
       >
-        <TextField
-          id="fiat-selector"
-          label="Select crypto"
-          variant="outlined"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          value={searchedCrypto}
-          onChange={handleChange}
+        <Autocomplete
+          multiple
+          id="checkboxes-tags-demo"
+          options={cryptos}
+          disableCloseOnSelect
+          getOptionLabel={(option) => option.symbol}
+          renderOption={(props, option, { selected }) => (
+            <li {...props}>
+              <Checkbox
+                icon={icon}
+                checkedIcon={checkedIcon}
+                style={{ marginRight: 8 }}
+                checked={selected}
+              />
+              {option.name + " (" + option.symbol.toUpperCase() + ")"}
+            </li>
+          )}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Select cryptocurrencies to track"
+              placeholder=""
+              InputLabelProps={{
+                shrink: true,
+              }}
+              value={searchedCrypto}
+              onChange={handleChange}
+            />
+          )}
         />
       </Box>
     </>
   );
-}
-function obj(obj: any): (value: any, index: number, array: any[]) => void {
-  throw new Error("Function not implemented.");
 }
