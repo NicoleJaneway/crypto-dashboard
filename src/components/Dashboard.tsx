3af4cc5ec45917adccdeee0287f09f5aa51119b7
Nicole Janeway Bills <nicolejanewaybills@gmail.com>
2021-11-27 05:24:49 -0600

    refactor pricing table

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,99 @@
+import { useState, useEffect, useContext } from "react";
+
 import FiatSelector from "./FiatSelector";
 import PriceTable from "./PriceTable";
 import CryptoSearch from "./CryptoSearch";
 
 import useFetch from "../helpers/useFetch";
+import { FiatContext } from "../helpers/FiatContext";
 
+function createData(
+  rank: number,
+  id: string,
+  name: string,
+  symbol: string,
+  price: number,
+  percChange: number,
+  marketCap: number,
+  supply: number
+) {
+  return { rank, id, name, symbol, price, percChange, marketCap, supply };
+}
 
 export default function Dashboard() {
+  const { get } = useFetch("https://api.coingecko.com/api/v3/");
+  const { fiat } = useContext(FiatContext);
+  const [market, setMarket] = useState([]);
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    get(`coins/markets?vs_currency=${fiat}`)
+      .then(
+        (
+          data: {
+            id: string;
+            symbol: string;
+            name: string;
+            image: string;
+            current_price: number;
+            market_cap: number;
+            market_cap_rank: number;
+            fully_diluted_valuation: number;
+            total_volume: number;
+            high_24h: number;
+            low_24h: number;
+            price_change_24h: number;
+            price_change_percentage_24h: number;
+            market_cap_change_24h: number;
+            market_cap_change_percentage_24h: number;
+            circulating_supply: number;
+            total_supply: number;
+            max_supply: number;
+            ath: number;
+            ath_change_percentage: number;
+            ath_date: string;
+            atl: number;
+            atl_change_percentage: number;
+            atl_date: string;
+            roi: {
+              times: number;
+              currency: string;
+              percentage: number;
+            };
+            last_updated: number;
+          }[]
+        ): void => {
+          setMarket(data);
+        }
+      )
+      .catch((error) => console.log("Could not load crypto", error));
+  }, [fiat]);
+
+  useEffect(() => {
+    const makeTable = (input) => {
+      let info = [];
+      for (let rank = 0; rank < 5; rank++) {
+        let row = createData(
+          rank + 1,
+          input[rank].id,
+          input[rank].name,
+          input[rank].symbol,
+          input[rank].current_price,
+          input[rank].price_change_percentage_24h,
+          input[rank].market_cap,
+          input[rank].circulating_supply
+        );
+        info.push(row);
+      }
+      return info;
+    };
+
+    if (market.length > 0) {
+      setRows(makeTable(market));
+      //   console.log(makeTable(market));
+    }
+  }, [market, fiat]);
+
   return (
     <>
       <div
@@ -22,7 +110,7 @@ export default function Dashboard() {
           <FiatSelector />
         </div>
       </div>
-      <PriceTable />
+      <PriceTable rows={rows} />
     </>
   );
 }
--- src/components/PriceTable.tsx
+++ src/components/PriceTable.tsx
@@ -7,121 +7,14 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Tooltip from "@mui/material/Tooltip";
 
-import { useState, useEffect, useContext } from "react";
-import useFetch from "../helpers/useFetch";
-import Loader from "../helpers/Loader";
 import { FiatContext } from "../helpers/FiatContext";
+import { useContext } from "react";
 
-function createData(
-  rank: number,
-  id: string,
-  name: string,
-  symbol: string,
-  price: number,
-  percChange: number,
-  marketCap: number,
-  supply: number
-) {
-  return { rank, id, name, symbol, price, percChange, marketCap, supply };
-}
-
-export default function PriceTable() {
-  const [cryptos, setCryptos] = useState([]);
-  const [marketData, setMarketData] = useState([]);
-  const { get, loading } = useFetch("https://api.coingecko.com/api/v3/");
-  const { post } = useFetch("http://localhost:8080/");
+export default function PriceTable({ rows }) {
   const { fiat } = useContext(FiatContext);
-  const [rows, setRows] = useState([]);
-
-  useEffect(() => {
-    get(`coins/markets?vs_currency=${fiat}`)
-      .then(
-        (
-          data: {
-            id: string;
-            symbol: string;
-            name: string;
-            image: string;
-            current_price: number;
-            market_cap: number;
-            market_cap_rank: number;
-            fully_diluted_valuation: number;
-            total_volume: number;
-            high_24h: number;
-            low_24h: number;
-            price_change_24h: number;
-            price_change_percentage_24h: number;
-            market_cap_change_24h: number;
-            market_cap_change_percentage_24h: number;
-            circulating_supply: number;
-            total_supply: number;
-            max_supply: number;
-            ath: number;
-            ath_change_percentage: number;
-            ath_date: string;
-            atl: number;
-            atl_change_percentage: number;
-            atl_date: string;
-            roi: {
-              times: number;
-              currency: string;
-              percentage: number;
-            };
-            last_updated: number;
-          }[]
-        ): void => {
-          setCryptos(data);
-        }
-      )
-      .catch((error) => console.log("Could not load crypto", error));
-  }, [fiat]);
-
-  useEffect(() => {
-    if (cryptos.length > 0) {
-      const makeMarketData = (data) => {
-        let info = [];
-        info = data.filter((obj) => obj.market_cap_rank > 5);
-        return info;
-      };
-      setMarketData(makeMarketData(cryptos));
-    }
-  }, [cryptos]);
-
-  useEffect(() => {
-    if (marketData.length > 0) {
-      post("marketdata", {
-        body: marketData,
-      }).catch((error) => console.log(error));
-    }
-  }, [marketData]);
-
-  useEffect(() => {
-    const makeTable = (input) => {
-      let info = [];
-      for (let rank = 0; rank < 5; rank++) {
-        let row = createData(
-          rank + 1,
-          input[rank].id,
-          input[rank].name,
-          input[rank].symbol,
-          input[rank].current_price,
-          input[rank].price_change_percentage_24h,
-          input[rank].market_cap,
-          input[rank].circulating_supply
-        );
-        info.push(row);
-      }
-      return info;
-    };
-
-    if (cryptos.length > 0) {
-      setRows(makeTable(cryptos));
-    }
-  }, [cryptos, fiat]);
 
   return (
     <TableContainer component={Paper}>
-      {loading && <Loader />}
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
