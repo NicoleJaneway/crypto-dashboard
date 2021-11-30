483d60bb178184fe6b1b42a2b2152b1e29692a47
@NicoleJaneway <44853527+NicoleJaneway@users.noreply.github.com>
2021-11-30 17:04:53 -0600

    test

--- src/controller/LoadTableController.tsx
+++ src/controller/LoadTableController.tsx
@@ -2,7 +2,7 @@ import Button from "@mui/material/Button";
 import useFetch from "../helpers/useFetch";
 
 export default function LoadTable({ setTrackedCrypto }): JSX.Element {
-  const { get } = useFetch("/");
+  const { get } = useFetch("http://localhost:57145/");
 
   const handleClick = () => {
     get("checked")
