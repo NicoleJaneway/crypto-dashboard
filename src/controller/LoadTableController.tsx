e3772d1476fdf066382c581de5a6ba5b9ecbdd73
Nicole Janeway <bnicolebills@gmail.com>
2021-12-04 08:39:27 -0600

    update

--- src/controller/LoadTableController.tsx
+++ src/controller/LoadTableController.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import useFetch from "../helpers/useFetch";
 
 export default function LoadTable({ setTrackedCrypto }): JSX.Element {
-  const { get } = useFetch("http://localhost:80/");
+  const { get } = useFetch("/");
 
   useEffect(() => {
     get("checked")
--- src/controller/TrackedCryptoController.tsx
+++ src/controller/TrackedCryptoController.tsx
@@ -13,7 +13,7 @@ export default function TrackedCryptoController({
   const [display, setDisplay] = useState<
     { name: string; symbol: string; id: string }[]
   >([]);
-  const { get, post, del } = useFetch("http://localhost:80/");
+  const { get, post, del } = useFetch("/");
 
   useEffect(() => {
     get("checked")
